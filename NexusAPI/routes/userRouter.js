8ef4b2b79c3e13087aec5919c0c6ff03022e0dfb
Abdelrhman Hany <mindenslavement@gmail.com>
2021-11-09 01:46:08 +0200

    initial

---
+++ NexusAPI/routes/userRouter.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var userRouter = express.Router();
+
+/* GET users listing. */
+userRouter.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = userRouter;
