9f689852831b37c042c1e4b5bacdb6b040883a15
Abdelrhman Hany <mindenslavement@gmail.com>
2021-11-09 20:54:41 +0200

    test real scenario 2

--- NexusAPI/routes/userRouter.js
+++ NexusAPI/routes/userRouter.js
@@ -6,4 +6,8 @@ userRouter.get('/', function(req, res, next) {
   res.json({res:'respond with a resource'});
 });
 
+userRouter.get('/d', function(req, res, next) {
+  res.json({res:'respond with a resource'});
+});
+
 module.exports = userRouter;
