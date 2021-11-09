7baba40803089211303c77463eb246b654a54727
Abdelrhman Hany <mindenslavement@gmail.com>
2021-11-09 16:26:51 +0200

    adding CI should succeed test

---
+++ NexusAPI/test/tests.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+const request = require('supertest');
+chai.should();
+chai.use(chaiHttp);
+
+
+describe("Initial test", ()=>{
+    describe("Sign up", ()=>{
+        it("should show output", (done)=>{
+            request(server)
+            .get('/users')
+            .end((err, res)=>{
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property("res", "respond with a resource");
+                done();
+            });
+        });
+    })
+});
