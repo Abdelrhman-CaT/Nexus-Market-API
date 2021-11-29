45e7ba5aa4d692cd87d6e21910a11dc24b31bbd6
Abdelrhman Hany <mindenslavement@gmail.com>
2021-11-29 19:18:27 +0200

    Connecting to databases"

--- NexusAPI/config.js
+++ NexusAPI/config.js
@@ -5,5 +5,6 @@
 
 // Used to store configuration data
 module.exports = {
-    "offlineDbUrl": "mongodb://localhost:27017/x",
+    "db1": "mongodb+srv://cat:nexus@nexus1.l4mg9.mongodb.net/nexus?retryWrites=true&w=majority",
+    "db2": "mongodb+srv://cat:nexus@nexus2.y1bws.mongodb.net/nexus?retryWrites=true&w=majority"
 };
